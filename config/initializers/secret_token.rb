de3638e90279bef3e1eeddcbfeeb62cf11c785af
Ivan Calderon <iwano@hotmail.com>
2012-10-15 13:31:28 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CreativeCrowd::Application.config.secret_token = 'c91060d55698a39984aaa3b8f26923de3b21bc9365d7faf9e6852f02d3f41979eb6cf47dbfd76f6a9493992bda097e2cc9309f449c9b532a60693b1ef641c4a2'
