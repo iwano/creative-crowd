737e84a325a195d3142859f422d3545a94f72a97
Ivan Calderon <iwano@hotmail.com>
2012-10-15 22:17:56 -0500

    Style header navbar and add base stylesheet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,9 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+ def is_active(controller, action)  
+   return "active" if params[:controller] == controller && params[:action] == action
+ end
+
 end
